2d1540002d178902da54564ddc545ac868d7dcbf
srikanth Karra <srikanthreddy376@gmail.com>
2019-01-07 18:21:39 +0100

    added skills, contact

--- main.js
+++ main.js
@@ -28,4 +28,65 @@ $(function () {
         }
       });
     });
+
+
+ // Initialize Firebase
+ var config = {
+  apiKey: "AIzaSyA4o0s6YMv78_m-MivqAAmBwdU8EW6_p00",
+  authDomain: "portfolio-contact-edcad.firebaseapp.com",
+  databaseURL: "https://portfolio-contact-edcad.firebaseio.com",
+  projectId: "portfolio-contact-edcad",
+  storageBucket: "portfolio-contact-edcad.appspot.com",
+  messagingSenderId: "800087795947"
+};
+firebase.initializeApp(config);
+
+ //initializing firebase in app
+ var messageRef = firebase.database().ref('messages');
+
+ //form values
+document.getElementById('contact-me').addEventListener('submit',submitForm);
+
+function submitForm(e){
+   e.preventDefault();
+
+  const name = getInput('yourname');
+  const email = getInput('email');
+  const Telephone = getInput('telephone');
+  const companyname = getInput('companyname');
+  const website = getInput('website');
+  const position= getInput('position');
+  const message= getInput('message');
+ 
+
+  //function referenced to DOM
+  function getInput(id){
+    return document.getElementById(id).value;
+   }
+
+  //save values to data base
+  saveMessage(name,email,message, Telephone,companyname,website,position);
+  
+   document.querySelector("#showalert").style.display ="block";
+  setTimeout(function(){
+    document.querySelector("#showalert").style.display ="none";
+    document.getElementById('contact-me').reset(); 
+  },3000);
+  
+};
+
+//save message to fire base
+  function saveMessage(name,email,message,Telephone,companyname,position,website) {
+  const newMessageRef = messageRef.push();
+  newMessageRef.set({
+    name:name,
+    email:email,
+    Telephone:Telephone,
+    companyname:companyname,
+    website:website,
+    position: position,
+     message:message
+  });
+};
+
    
